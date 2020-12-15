a3dbce4cbf3911d62fc1d80061d248281919c2c5
mikeowino <mike.owino40@yahoo.com>
2020-12-15 11:43:44 +0300

    red

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-/*red = [0, 100, 63];
+// Color variables:
+red = [0, 100, 63];
 orange = [40, 100, 60];
 green = [75, 100, 40];
 blue = [196, 77, 55];
 purple = [280, 50, 60];
-*/
-// Change the text between the quotation marks and run your code to see your name animated!
-drawName('Mike Owino');
 
-// The code below will allow you to interact with the letters
-// drawName(message, purple);
+// Letters in the message will cycle through these colors:
+letterColors = [red, orange, green, blue, purple];
 
+message = 'Mike Owino';
+
+drawName(message, letterColors);
 bounceBubbles();
